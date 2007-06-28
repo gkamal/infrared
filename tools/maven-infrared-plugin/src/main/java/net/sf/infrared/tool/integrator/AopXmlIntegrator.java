59cddd35d4398a81c5343404adcdfe43d64748b7
chetan_oct <chetan_oct@ba3bc1d8-1b30-0410-a7f4-bfa3c3621199>
2007-06-28 05:15:27 +0000

    Fixed for copying location. Now aop.xml would be copied to WEB-INF/ folder in case of war.
    git-svn-id: https://infrared.svn.sourceforge.net/svnroot/infrared/infrared2/app/trunk@22 ba3bc1d8-1b30-0410-a7f4-bfa3c3621199

--- tools/maven-infrared-plugin/src/main/java/net/sf/infrared/tool/integrator/AopXmlIntegrator.java
+++ tools/maven-infrared-plugin/src/main/java/net/sf/infrared/tool/integrator/AopXmlIntegrator.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright 2005 Tavant Technologies and Contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
 package net.sf.infrared.tool.integrator;
 
 import java.io.File;
@@ -14,6 +30,8 @@ import net.sf.infrared.tool.util.FileUtil;
  * It can be used for both web or ear.
  * 
  * @author chetanm
+ * @date Jun 22, 2007
+ * @version $Revision: 1.1 $
  */
 public class AopXmlIntegrator implements Integrator {
 	private static final Logger logger = Logger.getLogger(AopXmlIntegrator.class.getName());
@@ -32,10 +50,19 @@ public class AopXmlIntegrator implements Integrator {
 			return;
 		}
 		
-		File metainfdir = new File(app,"META-INF");
-		fileUtil.ensureDirectoryExists(metainfdir);
-		fileUtil.copyFileToDirectory(aopxml,metainfdir);
-		logger.fine("Copied aop.xml to "+metainfdir);
+		// Tomcat does not recognize war/META-INF/aop.xml for that put that in 
+		// WEB-INF/aop.xml or WEB-INF/classes/META-INF/aop.xml.
+		// For the rest copy that to META-INF/aop.xml
+		File toDir = null;
+		if (type == ArchiveType.WAR){
+			toDir = new File(app,"WEB-INF");
+		}else{
+			toDir = new File(app,"META-INF");
+		}
+		
+		fileUtil.ensureDirectoryExists(toDir);
+		fileUtil.copyFileToDirectory(aopxml,toDir);
+		logger.fine("Copied aop.xml to "+toDir);
 		
 		//TODO what if there already exist aop.xml. Should we merge them
 	}
